6e2b5cad3b30fe2cfcf2ed9620ff4463dfca16f3
AlenaAgeeva <alenapvageeva@gmail.com>
2022-05-28 22:04:45 -0300

    0.2 easy TwoSum

--- src/main/java/easy/TwoSum.java
+++ src/main/java/easy/TwoSum.java
@@ -6,9 +6,14 @@ package easy;
  * You may assume that each input would have exactly one solution,
  * and you may not use the same element twice.
  * You can return the answer in any order.
+ *
  * @author Alena Ageeva
  */
 public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        return new int[9];
+    }
+
     public static void main(String[] args) {
 
     }
