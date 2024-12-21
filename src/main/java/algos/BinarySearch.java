c921f6501152393e11b45eb2f498a935fddff613
Alena Ageeva <alenaageevapv@gmail.com>
2024-12-20 20:12:16 -0400

    Add binary search code and some tasks of this algorythm

---
+++ src/main/java/algos/BinarySearch.java
@@ -0,0 +1,42 @@
+package algos;
+
+public class BinarySearch {
+    public static int getCount(int[] arr, int num) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            if (arr[middle] == num) {
+                return middle;
+            } else if (num < arr[middle]) {
+                high = middle - 1;
+            } else {
+                low = middle + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int getCount(String[] arr, String name) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            if (name.equals(arr[middle])) {
+                return middle;
+            }
+            if (name.codePointAt(0) < arr[middle].codePointAt(0)) {
+                high = middle - 1;
+            } else {
+                low = middle + 1;
+            }
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+//        System.out.println(BinarySearch.getCount(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3));
+        System.out.println(BinarySearch.getCount(new String[]{"Abby", "Bob", "Grey", "Simon", "Tanuya", "Yan"}, "Yan"));
+    }
+}
---
+++ src/main/java/easy/array/MaximumCountOfPositiveIntegerAndNegativeInteger.java
@@ -0,0 +1,60 @@
+package easy.array;
+
+/**
+ * 2529. Maximum Count of Positive Integer and Negative Integer
+ * Given an array nums sorted in non-decreasing order, return the maximum between the number of positive integers
+ * and the number of negative integers.
+ * In other words, if the number of positive integers in nums is pos and the number of negative integers is neg,
+ * then return the maximum of pos and neg.
+ * Note that 0 is neither positive nor negative.
+ * Example 1:
+ * Input: nums = [-2,-1,-1,1,2,3]
+ * Output: 3
+ * Explanation: There are 3 positive integers and 3 negative integers. The maximum count among them is 3.
+ * Example 2:
+ * Input: nums = [-3,-2,-1,0,0,1,2]
+ * Output: 3
+ * Explanation: There are 2 positive integers and 3 negative integers. The maximum count among them is 3.
+ * Example 3:
+ * Input: nums = [5,20,66,1314]
+ * Output: 4
+ * Explanation: There are 4 positive integers and 0 negative integers. The maximum count among them is 4.
+ */
+public class MaximumCountOfPositiveIntegerAndNegativeInteger {
+    public static int maximumCount(int[] nums) {
+        int length = nums.length;
+        if (nums[0] == 0 && nums[length - 1] == 0) {
+            return 0;
+        }
+        if (nums[0] > 0 || nums[nums.length - 1] < 0) {
+            return nums.length;
+        }
+        int left = 0;
+        int right = length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        int negCount = left;
+        int posCount = length - negCount;
+        int zeroCount = 0;
+        for (int i = left; i < length; i++) {
+            if (nums[i] == 0) {
+                zeroCount++;
+            } else {
+                break;
+            }
+        }
+        return Math.max(negCount, posCount - zeroCount);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MaximumCountOfPositiveIntegerAndNegativeInteger
+                .maximumCount(new int[]{-2, -1, -1, -8, 0, 0, 0, 0, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14}));
+    }
+
+}
