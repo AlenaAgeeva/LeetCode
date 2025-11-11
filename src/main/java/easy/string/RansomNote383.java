7c21e5652fc7d26f07d71e98977d4446d7e6458c
Alena Ageeva <alenaageevapv@gmail.com>
2025-11-11 11:30:46 -0400

    Added new solution

--- src/main/java/easy/string/RansomNote383.java
+++ src/main/java/easy/string/RansomNote383.java
@@ -1,2 +1,38 @@
-package easy.string;public class RansomNote383 {
+package easy.string;
+
+/**
+ * Given two strings ransomNote and magazine, return true if ransomNote can
+ * be constructed by using the letters from magazine and false otherwise.
+ * Each letter in magazine can only be used once in ransomNote.
+ * Example 1:
+ * Input: ransomNote = "a", magazine = "b"
+ * Output: false
+ * Example 2:
+ * Input: ransomNote = "aa", magazine = "ab"
+ * Output: false
+ * Example 3:
+ * Input: ransomNote = "aa", magazine = "aab"
+ * Output: true
+ */
+public class RansomNote383 {
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+        int[] ransonArray = new int[125];
+        int[] magazineArray = new int[125];
+        for (int i = 0; i < ransomNote.length(); i++) {
+            ransonArray[ransomNote.charAt(i)]++;
+        }
+        for (int i = 0; i < magazine.length(); i++) {
+            magazineArray[magazine.charAt(i)]++;
+        }
+        for (int i = 0; i < ransonArray.length; i++) {
+            if (ransonArray[i] != 0
+                    && ransonArray[i] > magazineArray[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/test/java/easy/string/RansomNote383Test.java
+++ src/test/java/easy/string/RansomNote383Test.java
@@ -1,5 +1,28 @@
+package easy.string;
+
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
+
 class RansomNote383Test {
-  
+
+    @Test
+    void whenTrue() {
+        String input = "aa";
+        String input2 = "aab";
+        Assert.assertTrue(RansomNote383.canConstruct(input, input2));
+    }
+
+    @Test
+    void whenFalse() {
+        String input = "a";
+        String input2 = "b";
+        Assert.assertFalse(RansomNote383.canConstruct(input, input2));
+    }
+
+    @Test
+    void whenFalse2() {
+        String input = "aa";
+        String input2 = "ab";
+        Assert.assertFalse(RansomNote383.canConstruct(input, input2));
+    }
 }
